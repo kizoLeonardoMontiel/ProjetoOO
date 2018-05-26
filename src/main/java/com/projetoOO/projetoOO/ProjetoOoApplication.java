6354a2cb2ed93ec460b971f58c95a44a642d26c8
leonardo.montiel <leonardo.montiel@e-core.com>
2018-05-26 16:39:49 -0300

    Commit inicial

---
+++ src/main/java/com/projetoOO/projetoOO/ProjetoOoApplication.java
@@ -0,0 +1,12 @@
+package com.projetoOO.projetoOO;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoOoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoOoApplication.class, args);
+	}
+}
