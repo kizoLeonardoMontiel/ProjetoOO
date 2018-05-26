8b18e2210f43568b3267ed7ec2b7cb39539b7270
leonardo.montiel <leonardo.montiel@e-core.com>
2018-05-26 20:32:31 -0300

    mapeamento das URLs feito

--- src/main/java/com/projetoOO/projetoOO/projController/projController.java
+++ src/main/java/com/projetoOO/projetoOO/projController/projController.java
@@ -13,12 +13,81 @@ public class projController {
 	}
 	
 	@RequestMapping("/evento")
-	public String evento() {
+	public String eventoRequest() {
 		return "evento";
 	}
 	
 	@GetMapping("/evento/{ID}")
-	public String eventoID() {
+	public String eventoIDGet() {
 		return null;
+	}
+	
+	@PostMapping("/evento")
+	public String eventoPost() {
+		return null;
+	}
+	
+	@PostMapping("/evento/{ID}")
+	public String eventoIDPost() {
+		return null;
+	}
+	
+	@GetMapping("/evento/{ID}/delete")
+	public String eventoIDDeleteGet() {
+		return null;
+	}
+	@GetMapping("/usuario/{ID}")
+	public String usuarioIDGet() {
+		return null;
+	}
+	
+	@GetMapping("/usuario/cadastro")
+	public String usuarioCadastroGet() {
+		return null;
+	}
+	
+	@PostMapping("/usuario/cadastro")
+	public String usuarioCadastroPost(){
+		return null;
+	}
+
+	@GetMapping("/submissoes")
+	public String submissoesGet() {
+		return null;
+	}
+	
+	@RequestMapping("/submissoes")
+	public String submissoesRequest() {
+		return "submissoes";
+	}
+	
+	@GetMapping("/submissoes/{ID}")
+	public String submissoesIDGet() {
+		return null;
+	}
+	
+	@PostMapping("/submissoes/{ID}")
+	public String submissoesIDPost() {
+		return null;
+	}
+	
+	@PostMapping("/submissoes/{ID}/delete")
+	public String submissoesIDDeletePost() {
+		return null;
+	}
+	
+	@GetMapping("/submissoes/evento/{IDEvento}")
+	public String submissoesEventoIDGet() {
+		return null;
+	}
+	
+	@PostMapping("/submissoes/evento/{IDEvento}")
+	public String submissoesEventoIDPost() {
+		return null;
+	}
+	
+	@RequestMapping("/usuario")
+	public String usuarioRequest(){
+		return "usuario";
 	}
 }
