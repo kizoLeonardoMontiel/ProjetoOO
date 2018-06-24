7f12b9abadaf717c11d7a589dd006bb93649b220
Marcelo Machado <39655117+dmarcelomachado@users.noreply.github.com>
2018-06-24 16:46:59 -0300

    Atualização
    Atualização

--- src/main/resources/db/changelog/changes/0003.CREATE_TABLE_SUBMISSOES.sql
+++ src/main/resources/db/changelog/changes/0003.CREATE_TABLE_SUBMISSOES.sql
@@ -1,15 +1,15 @@
-create table Submissoes (
-  id bigint auto_increment not null,
-  even_id bigint not null,
-  usua_id bigint not null,
-  nome_art varchar(255) not null,
-  resumo_art varchar(255) not null,
-  dt_submissao datetime not null,
-  anexo_art varchar(255) not null,
-  primary key (id),
-  unique key UK_SUBMISSOES(nome_art),
-  unique key UK_EVENTO(even_id),
-  foreign key (even_id) references eventos(id),
-  unique key UK_USUARIO_SUBMISSAO(usua_id),
-  foreign key (usua_id) references usuario(id)
+ create table Submissoes (
+   id bigint auto_increment not null,
+   even_id bigint not null,
+   usua_id bigint not null,
+   nome_art varchar(255) not null,
+   resumo_art varchar(255) not null,
+   dt_submissao DATETIME not null,
+   anexo_art varchar(255) not null,
+   primary key (id),
+   unique key UK_SUBMISSOES(nome_art),
+   unique key UK_EVENTO(even_id),
+   foreign key (even_id) references eventos(id),
+   unique key UK_USUARIO_SUBMISSAO(usua_id),
+   foreign key (usua_id) references usuario(id)
 )
