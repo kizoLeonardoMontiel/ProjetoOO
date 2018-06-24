b3a161b0c0a7db0d3b829f7d8a1d537c0ca0de3e
Marcelo Machado <39655117+dmarcelomachado@users.noreply.github.com>
2018-06-24 16:41:28 -0300

    Atualização
    Atualização

--- src/main/java/com/projetoOO/projetoOO/config/stringConstants.java
+++ src/main/java/com/projetoOO/projetoOO/config/stringConstants.java
@@ -2,13 +2,8 @@ package com.projetoOO.projetoOO.config;
 
 public class stringConstants {
 
-    public static final String USER_LOGGED = "userLogged";
-    public static final String ADMIN = "admin";
     public static final String ERROR = "error";
     public static final String SUCCESS = "success";
-    public static final String ROLE_ADMIN = "ROLE_ADMIN";
-
-    public static final String ERROR_VINHO_NAO_EXISTE = "O vinho solicitado não existe.";
 
     private stringConstants(){}
 }
