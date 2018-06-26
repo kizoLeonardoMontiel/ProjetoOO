43f59fdb312045202009501a36bed00fe1fad053
Marcelo Machado <39655117+dmarcelomachado@users.noreply.github.com>
2018-06-25 22:58:14 -0300

    Correções e Implementações
    Correções e Implementações

---
+++ src/main/java/com/projetoOO/projetoOO/service/Services.java
@@ -0,0 +1,32 @@
+package com.projetoOO.projetoOO.service;
+
+import com.projetoOO.projetoOO.projModel.Eventos;
+import com.projetoOO.projetoOO.projRepository.EventosRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+
+@Service
+public class Services {
+
+    @Autowired
+    private EventosRepository repository;
+
+    public List<Eventos> findAll() {
+        return repository.findAll();
+    }
+
+    //public Eventos findOne(Long id) {
+    //    return repository.findOne(id);
+    //}
+
+   // public Eventos save(Eventos eventos) {
+    //    return repository.saveAndFlush(eventos);
+   // }
+
+    //public void delete(Long id) {
+    //    repository.delete(id);
+    //}
+}
