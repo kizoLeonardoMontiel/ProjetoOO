b310d6f8dcd62cc51b73240912097a085d39f4ff
Marcelo Machado <39655117+dmarcelomachado@users.noreply.github.com>
2018-06-26 21:19:41 -0300

    Correções de Rota e Ajustes HTML
    Correções de Rota e Ajustes HTML

--- src/main/java/com/projetoOO/projetoOO/projRepository/EventosRepository.java
+++ src/main/java/com/projetoOO/projetoOO/projRepository/EventosRepository.java
@@ -1,17 +1,14 @@
 package com.projetoOO.projetoOO.projRepository;
 
 import com.projetoOO.projetoOO.projModel.Eventos;
-//import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface EventosRepository extends JpaRepository<Eventos, Long> {
+public interface EventosRepository extends CrudRepository<Eventos, Long> {
 
     Eventos findById(long id);
 	Eventos findByEventos(String nome_evento);
-
-
 }
